02139934c4e3cb59f77dde49917c62e006ac2a4e
Upasana-99 <81972877+Upasana-99@users.noreply.github.com>
2021-04-13 19:39:26 +0530

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,35 @@
+const mongoose=require("mongoose");
+
+const contactSchema=new mongoose.Schema({
+    firstname :{
+        type:String,
+        required:true
+
+    },
+    lastname :{
+        type:String,
+        required:true
+
+    },
+    email :{
+        type:String,
+        required:true,
+        unique:true
+
+    },
+    phone :{
+        type:String,
+        required:true
+
+    },
+    message :{
+        type:String,
+        required:true
+
+    }
+
+})
+
+const Contact = new mongoose.model("Contact",contactSchema);
+
+module.exports = Contact;
